adb966a0a150927407a986d2de214a0ea0660649
sarahjx <swang311@my.bcit.ca>
2026-01-20 13:29:48 -0800

    Fix: Add missing useRef import in CreateRoomModal

--- src/components/CreateRoomModal.js
+++ src/components/CreateRoomModal.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import Modal from './Modal';
 import Button from './Button';
